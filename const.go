b9e5cf0bbcd754912c9ee09a66c78ab70b4c19a3
GitiRay <84114630+gitiray@users.noreply.github.com>
2021-10-01 20:41:59 +0800

    Optimize info on Telegraph

--- const.go
+++ const.go
@@ -5,7 +5,7 @@ import "github.com/MakeGolangGreat/telegraph-go"
 const (
 	projectLink = "https://github.com/MakeGolangGreat/ArchiveBot"
 	projectName = "MakeGolangGreat"
-	projectDesc = `<blockquote>本页面由开源程序「<a href="https://github.com/MakeGolangGreat/ArchiveBot">ArchiveBot</a>」生成，内容来自网络。</blockquote>`
+	projectDesc = `<blockquote>本页面由 <a href="https://github.com/MakeGolangGreat/ArchiveBot">ArchiveBot</a> 生成，内容来自网络。</blockquote>`
 	apiEndpoint = "https://api.telegram.org/bot%s/%s"
 )
 
@@ -20,22 +20,7 @@ var attachInfo = &telegraph.NodeElement{
 			Children: []telegraph.Node{
 				telegraph.NodeElement{
 					Tag:      "strong",
-					Children: []telegraph.Node{"本页面由 "},
-				},
-				telegraph.NodeElement{
-					Tag: "a",
-					Attrs: map[string]string{
-						"href": "https://t.me/beifenbot",
-					},
-					Children: []telegraph.Node{"@beifenbot"},
-				},
-				telegraph.NodeElement{
-					Tag:      "strong",
-					Children: []telegraph.Node{" 备份，"},
-				},
-				telegraph.NodeElement{
-					Tag:      "strong",
-					Children: []telegraph.Node{"代码开源："},
+					Children: []telegraph.Node{"本页面由 ArchiveBot 生成，内容来自网络。代码开源："},
 				},
 				telegraph.NodeElement{
 					Tag: "a",
